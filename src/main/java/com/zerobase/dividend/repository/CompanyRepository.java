9172e1ace78d0ae4526f3c5e51f068de9f12be71
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 17:52:59 +0900

    feat: CompanyRepository-회사 이름으로 회사 정보 조회하는 쿼리 메서드 추가
    - findByName

--- src/main/java/com/zerobase/dividend/repository/CompanyRepository.java
+++ src/main/java/com/zerobase/dividend/repository/CompanyRepository.java
@@ -3,6 +3,10 @@ package com.zerobase.dividend.repository;
 import com.zerobase.dividend.domain.CompanyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
     boolean existsByTicker(String ticker);
+
+    Optional<CompanyEntity> findByName(String name);
 }
