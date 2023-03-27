3d8028cabf4143e32f0456ab2ebc8e2dfd5a6982
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 19:00:02 +0900

    feat: CompanyRepository-자동 저장 리스트 반환에서 LIKE 문을 이용한 회사명 찾기 쿼리 메서드 추가
    - findByNameStartingWithIgnoreCase

--- src/main/java/com/zerobase/dividend/repository/CompanyRepository.java
+++ src/main/java/com/zerobase/dividend/repository/CompanyRepository.java
@@ -1,6 +1,8 @@
 package com.zerobase.dividend.repository;
 
 import com.zerobase.dividend.domain.CompanyEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +11,6 @@ public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
     boolean existsByTicker(String ticker);
 
     Optional<CompanyEntity> findByName(String name);
+
+    Page<CompanyEntity> findByNameStartingWithIgnoreCase(String keyword, Pageable pageable);
 }
