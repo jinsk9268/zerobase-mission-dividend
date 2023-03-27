9e123faf6d606708c2d740738446da97fd395d18
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 16:34:58 +0900

    refactor: Scraper-기존 ScraperInterface 인터페이스명 변경
    - ScraperInterface -> Scraper

--- src/main/java/com/zerobase/dividend/scraper/Scraper.java
+++ src/main/java/com/zerobase/dividend/scraper/Scraper.java
@@ -3,7 +3,7 @@ package com.zerobase.dividend.scraper;
 import com.zerobase.dividend.dto.CompanyDto;
 import com.zerobase.dividend.dto.ScrapedResult;
 
-public interface ScraperInterface {
+public interface Scraper {
     ScrapedResult scrap(CompanyDto companyDto);
     CompanyDto scrapCompanyByTicker(String ticker);
 }
