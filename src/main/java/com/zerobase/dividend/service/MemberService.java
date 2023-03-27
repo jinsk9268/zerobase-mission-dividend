b3c2ff3ce154019e064c2ab5e402e21dc5dc0afe
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 21:13:08 +0900

    feat: MemberService-loadUserByUsername 구현

---
+++ src/main/java/com/zerobase/dividend/service/MemberService.java
@@ -0,0 +1,20 @@
+package com.zerobase.dividend.service;
+
+import com.zerobase.dividend.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class MemberService implements UserDetailsService {
+    private MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다. -> " + username));
+    }
+}
