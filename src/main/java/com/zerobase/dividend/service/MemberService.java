5edb8dd0bca2fa87d5a717100bfcfe293167ed95
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 21:15:07 +0900

    feat: MemberService-회원 가입 기능 구현
    - register

--- src/main/java/com/zerobase/dividend/service/MemberService.java
+++ src/main/java/com/zerobase/dividend/service/MemberService.java
@@ -1,20 +1,36 @@
 package com.zerobase.dividend.service;
 
+import com.zerobase.dividend.domain.MemberEntity;
+import com.zerobase.dividend.dto.Auth;
 import com.zerobase.dividend.repository.MemberRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class MemberService implements UserDetailsService {
     private MemberRepository memberRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return memberRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다. -> " + username));
+    }
+
+    public MemberEntity register(Auth.SignUp member) {
+        boolean exists = this.memberRepository.existsByUsername(member.getUsername());
+
+        if (exists) {
+            throw new RuntimeException("이미 사용중인 아이디 입니다. -> " + member.getUsername());
+        }
+
+        member.setPassword(passwordEncoder.encode(member.getPassword()));
+
+        return memberRepository.save(member.toEntity());
     }
 }
