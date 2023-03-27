8cd036d52f45114a8702bf79eab9e512824a4ce6
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 21:20:24 +0900

    feat: TokenProvider-토큰 생성 메서드 구현
    - generateToken

---
+++ src/main/java/com/zerobase/dividend/security/TokenProvider.java
@@ -0,0 +1,39 @@
+package com.zerobase.dividend.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class TokenProvider {
+    private static final String KEY_ROLES = "roles";
+    private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60; // 1 hour
+
+    @Value("${spring.jwt.secret}")
+    private String secretKey;
+
+    /**
+     * 토큰 생성(발급)
+     */
+    public String generateToken(String username, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(KEY_ROLES, roles);
+
+        Date now = new Date();
+        Date expiredDate = new Date(now.getTime() + TOKEN_EXPIRE_TIME);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiredDate)
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
+    }
+}
