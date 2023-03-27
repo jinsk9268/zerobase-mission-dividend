4bf191114c3a272fe011dd6ad568d7983d131c7f
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 21:42:59 +0900

    feat: CompanyController-회사 삭제 DELETE API 요청, 캐시 삭제 메서드 구현
    - deleteCompany
    - clearFinanceCache

--- src/main/java/com/zerobase/dividend/controller/CompanyController.java
+++ src/main/java/com/zerobase/dividend/controller/CompanyController.java
@@ -2,7 +2,9 @@ package com.zerobase.dividend.controller;
 
 import com.zerobase.dividend.dto.CompanyDto;
 import com.zerobase.dividend.service.CompanyService;
+import com.zerobase.dividend.type.CacheKey;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.CacheManager;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class CompanyController {
     private final CompanyService companyService;
+    private final CacheManager redisCacheManager;
 
     /**
      * 회사 추가 (회사 정보 및 배당금)
@@ -47,5 +50,22 @@ public class CompanyController {
     @PreAuthorize("hasRole('WRITE')")
     public ResponseEntity<?> searchAutocomplete(String keyword) {
         return ResponseEntity.ok(companyService.getCompanyNamesByKeyword(keyword));
+    }
+
+    /**
+     * 회사 삭제 (회사와 관련된 모든 정보 삭제)
+     */
+    @DeleteMapping("/{ticker}")
+    @PreAuthorize("hasRole('WRITE')")
+    public ResponseEntity<?> deleteCompany(@PathVariable String ticker) {
+        String companyName = companyService.deleteCompany(ticker);
+
+        clearFinanceCache(companyName);
+
+        return ResponseEntity.ok(companyName);
+    }
+
+    public void clearFinanceCache(String companyName) {
+        redisCacheManager.getCache(CacheKey.KEY_FINANCE).evict(companyName );
     }
 }
