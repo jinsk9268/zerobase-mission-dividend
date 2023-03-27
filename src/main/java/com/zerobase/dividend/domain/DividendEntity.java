b73cbee58611981912d54c43676c9aa7f755e1db
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 14:02:33 +0900

    feat: DividendEntity-DB 테이블 클래스 생성

---
+++ src/main/java/com/zerobase/dividend/domain/DividendEntity.java
@@ -0,0 +1,24 @@
+package com.zerobase.dividend.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "dividend")
+public class DividendEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long companyId;
+    private LocalDateTime date;
+    private String dividend;
+}
