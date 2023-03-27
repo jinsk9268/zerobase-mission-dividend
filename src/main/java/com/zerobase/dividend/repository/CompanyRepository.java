ef2ad6d14449babdbcab60eda51e465e00f52079
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 17:05:59 +0900

    feat: CompanyRepository-ticker로 회사 존재 여부 확인 쿼리 메서드 추가
    - existsByTicker

--- src/main/java/com/zerobase/dividend/repository/CompanyRepository.java
+++ src/main/java/com/zerobase/dividend/repository/CompanyRepository.java
@@ -4,4 +4,5 @@ import com.zerobase.dividend.domain.CompanyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
+    boolean existsByTicker(String ticker);
 }
