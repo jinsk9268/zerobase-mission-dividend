e7ee8ae4d3a3076cf00009cfe4e7fea0080e9620
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 22:29:19 +0900

    feat: ErrorResponse-에러 응답 객체 생성

---
+++ src/main/java/com/zerobase/dividend/error/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.zerobase.dividend.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private int code;
+    private String message;
+}
