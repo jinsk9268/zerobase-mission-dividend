8370c51140d34c5f55e6687345d5b4226dc3074f
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 21:38:10 +0900

    feat: DividendRepository-companyId로 회사의 배당금 정보 삭제하는 쿼리 메서드 추가
    - deleteAllByCompanyId

--- src/main/java/com/zerobase/dividend/repository/DividendRepository.java
+++ src/main/java/com/zerobase/dividend/repository/DividendRepository.java
@@ -2,12 +2,16 @@ package com.zerobase.dividend.repository;
 
 import com.zerobase.dividend.domain.DividendEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface DividendRepository extends JpaRepository<DividendEntity, Long> {
     List<DividendEntity> findAllByCompanyId(Long companyId);
+
+    @Transactional
+    void deleteAllByCompanyId(Long id);
 
     boolean existsByCompanyIdAndDate(Long companyId, LocalDateTime date);
 }
