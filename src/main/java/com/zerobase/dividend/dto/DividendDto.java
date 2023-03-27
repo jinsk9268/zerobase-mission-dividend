3012d24b8c62b007b212de873d6f33f62b491c79
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 20:27:02 +0900

    feat: DividendDto-LocalDateTime 타입 레디스 캐시 사용시 직렬화, 역직렬화 설정 추가

--- src/main/java/com/zerobase/dividend/dto/DividendDto.java
+++ src/main/java/com/zerobase/dividend/dto/DividendDto.java
@@ -1,5 +1,9 @@
 package com.zerobase.dividend.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +16,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DividendDto {
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime date;
     private String dividend;
 }
