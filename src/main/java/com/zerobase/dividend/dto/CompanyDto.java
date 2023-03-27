1025eee834509c32ec44f409ac57c4025f362c41
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 14:06:01 +0900

    feat: CompanyDto-DB View를 위한 Dto 생성

---
+++ src/main/java/com/zerobase/dividend/dto/CompanyDto.java
@@ -0,0 +1,11 @@
+package com.zerobase.dividend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CompanyDto {
+    private String ticker;
+    private String name;
+}
