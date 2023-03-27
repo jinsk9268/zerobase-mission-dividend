baa5fb42459d5f2a2ac9e6e02c60f4564378c009
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 15:29:20 +0900

    feat: CompanyDto-롬복 생성자 어노테이션 추가

--- src/main/java/com/zerobase/dividend/dto/CompanyDto.java
+++ src/main/java/com/zerobase/dividend/dto/CompanyDto.java
@@ -1,10 +1,14 @@
 package com.zerobase.dividend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CompanyDto {
     private String ticker;
     private String name;
