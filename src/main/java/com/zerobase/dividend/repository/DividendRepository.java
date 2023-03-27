da33053a590fb19da272e1c3dc221b363fbf49f1
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 17:51:32 +0900

    feat: DividendRepository-회사 아이디의 모든 배당금 정보를 조회하는 쿼리 메서드 추가
    - findAllByCompanyId

--- src/main/java/com/zerobase/dividend/repository/DividendRepository.java
+++ src/main/java/com/zerobase/dividend/repository/DividendRepository.java
@@ -3,5 +3,8 @@ package com.zerobase.dividend.repository;
 import com.zerobase.dividend.domain.DividendEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DividendRepository extends JpaRepository<DividendEntity, Long> {
+    List<DividendEntity> findAllByCompanyId(Long companyId);
 }
