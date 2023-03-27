11cc01fe349a45d920f0bf3dbab733a7ee9e0691
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 21:28:02 +0900

    feat: AuthController-로그인 Post API 요청 구현
    - signin

--- src/main/java/com/zerobase/dividend/controller/AuthController.java
+++ src/main/java/com/zerobase/dividend/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.zerobase.dividend.controller;
 
+import com.zerobase.dividend.domain.MemberEntity;
 import com.zerobase.dividend.dto.Auth;
 import com.zerobase.dividend.security.TokenProvider;
 import com.zerobase.dividend.service.MemberService;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class AuthController {
     private final MemberService memberService;
+    private final TokenProvider tokenProvider;
 
     /**
      * 회원가입
@@ -25,6 +27,20 @@ public class AuthController {
     public ResponseEntity<?> signup(@RequestBody Auth.SignUp request) {
         return ResponseEntity.ok(
                 this.memberService.register(request)
+        );
+    }
+
+    /**
+     * 로그인 API
+     */
+    @PostMapping("/signin")
+    public ResponseEntity<?> signin(@RequestBody Auth.SignIn request) {
+        // 사용자한테 입력받은 아이디, 패스워드 검증
+        MemberEntity member = memberService.authenticate(request);
+
+        // 일치한다면 jwt 토큰 생성 후 반환
+        return ResponseEntity.ok(
+                tokenProvider.generateToken(member.getUsername(), member.getRoles())
         );
     }
 }
