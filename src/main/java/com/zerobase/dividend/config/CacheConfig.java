1a016ab1b372fb35414ef4957d24c58f6789de6b
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 20:17:45 +0900

    feat: CacheConfig-레디스 커넥션 맺을 수 있게 config 설정
    - redisConnectionFactory

---
+++ src/main/java/com/zerobase/dividend/config/CacheConfig.java
@@ -0,0 +1,28 @@
+package com.zerobase.dividend.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@AllArgsConstructor
+public class CacheConfig {
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration conf = new RedisStandaloneConfiguration();
+        conf.setHostName(host);
+        conf.setPort(port);
+
+        return new LettuceConnectionFactory(conf);
+    }
+}
