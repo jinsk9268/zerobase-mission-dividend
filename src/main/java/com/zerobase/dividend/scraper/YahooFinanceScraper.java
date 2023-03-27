66eedc0247439840d4182fe9fdfaaea3f114fcb3
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 16:08:50 +0900

    refactor: YahooFinanceScraper-ScraperConnection 상속으로 인한 메서드 수정
    - scrap
    - scrapCompanyByTicker

--- src/main/java/com/zerobase/dividend/scraper/YahooFinanceScraper.java
+++ src/main/java/com/zerobase/dividend/scraper/YahooFinanceScraper.java
@@ -4,84 +4,67 @@ import com.zerobase.dividend.dto.CompanyDto;
 import com.zerobase.dividend.dto.DividendDto;
 import com.zerobase.dividend.dto.ScrapedResult;
 import com.zerobase.dividend.type.Month;
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class YahooFinanceScraper implements ScraperInterface{
+public class YahooFinanceScraper extends ScraperConnection implements ScraperInterface {
     private final String SCRAPED_URL = "https://finance.yahoo.com/quote/%s/history?period1=%d&period2=%d&interval=1mo";
     private final String SUMMARY_URL = "https://finance.yahoo.com/quote/%s?p=%s";
     private final long START_DATE = 86400;
 
     @Override
     public ScrapedResult scrap(CompanyDto companyDto) {
-        ScrapedResult scrapedResult = new ScrapedResult();
-        scrapedResult.setCompanyDto(companyDto);
+        String url = String.format(
+                SCRAPED_URL,
+                companyDto.getTicker(),
+                START_DATE,
+                System.currentTimeMillis() / 1000
+        );
 
-        try {
-            long now = System.currentTimeMillis() / 1000;
-            String url = String.format(SCRAPED_URL, companyDto.getTicker(), START_DATE, now);
+        Elements parsingDivs = getScrapedDocument(url)
+                .getElementsByAttributeValue("data-test", "historical-prices");
+        Element table = parsingDivs.get(0);
+        Element tbody = table.children().get(1);
 
-            Connection connection = Jsoup.connect(url);
-            Document document = connection.get();
+        List<DividendDto> dividendDtoList = new ArrayList<>();
+        for (Element e : tbody.children()) {
+            String txt = e.text();
 
-            Elements parsingDivs = document.getElementsByAttributeValue("data-test", "historical-prices");
-            Element table = parsingDivs.get(0);
-            Element tbody = table.children().get(1);
-
-            List<DividendDto> dividendDtoList = new ArrayList<>();
-            for (Element e : tbody.children()) {
-                String txt = e.text();
-
-                if (!txt.endsWith("Dividend")) {
-                    continue;
-                }
-
-                String[] splits = txt.split(" ");
-                int month = Month.strToNumber(splits[0]);
-                int day = Integer.parseInt(splits[1].replace(",", ""));
-                int year = Integer.parseInt(splits[2]);
-                String dividend = splits[3];
-
-                if (month < 0) {
-                    throw new RuntimeException("매칭되는 월이 없습니다. -> " + splits[0]);
-                }
-
-                dividendDtoList.add(new DividendDto(
-                        LocalDateTime.of(year, month, day, 0, 0),
-                        dividend
-                ));
+            if (!txt.endsWith("Dividend")) {
+                continue;
             }
 
-            scrapedResult.setDividendDtoList(dividendDtoList);
-        } catch (IOException e) {
-            throw new RuntimeException("배당금 정보 스크래핑중 에러가 발생했습니다. -> " + e);
+            String[] splits = txt.split(" ");
+            int month = Month.strToNumber(splits[0]);
+            int day = Integer.parseInt(splits[1].replace(",", ""));
+            int year = Integer.parseInt(splits[2]);
+            String dividend = splits[3];
+
+            if (month < 0) {
+                throw new RuntimeException("매칭되는 월이 없습니다. -> " + splits[0]);
+            }
+
+            dividendDtoList.add(new DividendDto(
+                    LocalDateTime.of(year, month, day, 0, 0),
+                    dividend
+            ));
         }
 
-        return scrapedResult;
+        return new ScrapedResult(companyDto, dividendDtoList);
     }
 
     @Override
     public CompanyDto scrapCompanyByTicker(String ticker) {
         String url = String.format(SUMMARY_URL, ticker, ticker);
 
-        try {
-            Connection connection = Jsoup.connect(url);
-            Document document = connection.get();
+        Element h1 = getScrapedDocument(url)
+                .getElementsByTag("h1").get(0);
+        String title = h1.text().split(" - ")[1].trim();
 
-            Element h1 = document.getElementsByTag("h1").get(0);
-            String title = h1.text().split(" - ")[1].trim();
-
-            return new CompanyDto(ticker, title);
-        } catch (IOException e) {
-            throw new RuntimeException("회사 정보 스크래핑중 에러가 발생했습니다. -> " + e);
-        }
+        return new CompanyDto(ticker, title);
     }
 }
