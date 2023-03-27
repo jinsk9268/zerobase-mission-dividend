b4e357a8692abd84a492b8122913af0c5cd9623e
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 16:57:28 +0900

    feat: CompanyService-ticker 기준으로 DB에 회사 정보, 배당금 정보 저장 구현
    - storeCompanyAndDividend

---
+++ src/main/java/com/zerobase/dividend/service/CompanyService.java
@@ -0,0 +1,50 @@
+package com.zerobase.dividend.service;
+
+import com.zerobase.dividend.domain.CompanyEntity;
+import com.zerobase.dividend.domain.DividendEntity;
+import com.zerobase.dividend.dto.CompanyDto;
+import com.zerobase.dividend.dto.ScrapedResult;
+import com.zerobase.dividend.repository.CompanyRepository;
+import com.zerobase.dividend.repository.DividendRepository;
+import com.zerobase.dividend.scraper.Scraper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
+public class CompanyService {
+    private final Scraper yahooFinanceScraper;
+
+    private final CompanyRepository companyRepository;
+    private final DividendRepository dividendRepository;
+
+    /**
+     * ticker 기준으로 회사, 배당금 정보 저장
+     */
+    private CompanyDto storeCompanyAndDividend(String ticker) {
+        // 스크래핑 결과 가져오기
+        CompanyDto companyDto = yahooFinanceScraper.scrapCompanyByTicker(ticker);
+
+        if (ObjectUtils.isEmpty(companyDto)) {
+            throw new RuntimeException("스크랩에 실패했습니다. -> " + ticker);
+        }
+
+        ScrapedResult scrapedResult = yahooFinanceScraper.scrap(companyDto);
+
+        // 회사 저장
+        CompanyEntity companyEntity = companyRepository.save(new CompanyEntity(companyDto));
+
+        // 회사의 배당금 정보 저장
+        List<DividendEntity> dividendEntities
+                = scrapedResult.getDividendDtoList().stream()
+                .map(e -> new DividendEntity(companyEntity.getId(), e))
+                .collect(Collectors.toList());
+        dividendRepository.saveAll(dividendEntities);
+
+        return companyDto;
+    }
+}
