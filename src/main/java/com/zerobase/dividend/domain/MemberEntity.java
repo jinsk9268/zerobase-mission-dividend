6b1f7fb81f70e590da2a7ebc009a35858263873d
jinsk9268 <jinsk9268@gmail.com>
2023-03-27 20:55:36 +0900

    feat: MemberEntity-member DB 테이블 클래스 생성

---
+++ src/main/java/com/zerobase/dividend/domain/MemberEntity.java
@@ -0,0 +1,54 @@
+package com.zerobase.dividend.domain;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "member")
+public class MemberEntity implements UserDetails {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new) // 스프링 시큐리티에서 지원하는 role을 쓰기위해 맵핑
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
